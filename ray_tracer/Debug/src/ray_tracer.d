3481f099b151f6ee8d2e6bef51aa52ea7a5f55af
Fabian <lichterschalter@gmx.at>
2015-12-10 18:01:01 +0100

    folder structure, c++ hello world programm, libraries are downloaded

---
+++ ray_tracer/Debug/src/ray_tracer.d
@@ -0,0 +1,1 @@
+src/ray_tracer.o: ../src/ray_tracer.cpp
---
+++ ray_tracer/src/ray_tracer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ray_tracer.cpp
+// Author      : Fabian Türk
+// Version     : 1
+// Copyright   : Copyright by Fabian Türk
+// Description : Main Class
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
