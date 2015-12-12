46cdd7a5d6fee787e1676b694bc44fa19975db99
Fabian <lichterschalter@gmx.at>
2015-12-12 20:25:20 +0100

    writing simple .ppm file works

--- ray_tracer/RayTracer.cpp
+++ ray_tracer/RayTracer.cpp
@@ -6,11 +6,21 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include "RayTracer.h"
 using namespace std;
 
 int main() {
-
-	cout << "hola";
+	ofstream myfile;
+	myfile.open("example.ppm");
+	myfile << "P3 \n \
+		# sample.ppm \n \
+		4 4 \n \
+		15 \n \
+		 0  0  0    0  0  0    0  0  0   15  0 15 \n \
+		 0  0  0    0 15  7    0  0  0    0  0  0 \n \
+		 0  0  0    0  0  0    0 15  7    0  0  0 \n \
+		15  0 15    0  0  0    0  0  0    0  0  0";
 }
 
 
---
+++ ray_tracer/RayTracer.h
@@ -0,0 +1,15 @@
+/*
+ * RayTracer.h
+ *
+ *  Created on: 12.12.2015
+ *      Author: rustong_pu
+ */
+
+#ifndef RAYTRACER_H_
+#define RAYTRACER_H_
+
+class RayTracer{
+
+};
+
+#endif /* RAYTRACER_H_ */
