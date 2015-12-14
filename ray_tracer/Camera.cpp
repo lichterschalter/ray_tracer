02314dda76175d8bd427e2bd7893126882c243bf
Fabian <lichterschalter@gmx.at>
2015-12-14 14:45:57 +0100

    instance vars for camera are set; first use of the glm library - and it works

--- ray_tracer/Camera.cpp
+++ ray_tracer/Camera.cpp
@@ -6,12 +6,23 @@
  */
 
 #include <iostream>
+#include "../libs/glm/glm/vec4.hpp"
 #include "Camera.h"
 using namespace std;
 
-Camera::Camera(){ };
+Camera::Camera(){
+	position = glm::vec4 ( 0, 0, 0, 0 );
+	up = glm::vec4 ( 0, 1, 0, 0 );
+	horizontal_fov = 45;
+	vertical_fov = 45;
+};
 Camera::~Camera(){ };
-Camera::Camera( const Camera& ){ };
+Camera::Camera( const Camera& ){
+	position = glm::vec4 ( 0, 0, 0, 0 );
+	up = glm::vec4 ( 0, 1, 0, 0 );
+	horizontal_fov = 45;
+	vertical_fov = 45;
+};
 Camera& Camera::operator=( const Camera& ){ return *this; };
 
 
--- ray_tracer/Camera.h
+++ ray_tracer/Camera.h
@@ -8,7 +8,15 @@
 #ifndef CAMERA_H_
 #define CAMERA_H_
 
+#include "../libs/glm/glm/fwd.hpp"
+#include "../libs/glm/glm/vec4.hpp"
+
 class Camera{
+	glm::vec4 position;
+	glm::vec4 up;
+	float horizontal_fov;
+	float vertical_fov;
+
 	Camera( const Camera& );
 	Camera& operator=( const Camera& );
 public:
